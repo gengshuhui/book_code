8edfa38e45349e4ea8bee3116ce5a41b348a2d9f
gengshuhui <shuhuigeng@163.com>
2023-01-24 15:15:07 +0000

    add 3.1.2

---
+++ onwebs/gcc-plugins/01/3.1.2/build2.sh
@@ -0,0 +1,8 @@
+# gcc 7.5 not work, gcc 9.4 work ok
+gcc target.c -g -fpatchable-function-entry=10,5 -o bin_patchable.elf
+gcc target.c -g -o bin.elf
+objdump -d bin_patchable.elf > dis_patchable.s
+objdump -d bin.elf > dis.s
+
+diff -y dis.s dis_patchable.s
+
